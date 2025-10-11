33b8be6b4830f76ca91ff19f7e6de867be42b9e0
Aayansh Anand <95929174+aayanshanand@users.noreply.github.com>
2025-10-11 16:36:09 +0530

    first.js first commit

---
+++ first.js
@@ -0,0 +1,138 @@
+// console.log("Welcome to Apna college!");
+// console.log("I'm Aayansh Anand");
+// fullname = "Tony Stark";
+// console.log(fullname);
+// age = 24;
+// price = 99.99;
+// x = null;
+// y = undefined;
+// isFollow = true;
+// console.log(isFollow);
+
+// fullname = "Harsh";
+// FULLNAME = "Aayansh";
+
+// console.log(fullname);
+// console.log(FULLNAME);
+
+
+// let fullName = "Aayansh Anand";
+// let age = 22;
+// let price = 91.2;
+
+//  console.log(fullName);
+
+// {
+//     let a = 5;
+//     console.log(a);
+// }
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+
+// const student = {
+//     fullName : "Harsh",
+//     age : 22,
+//     cgpa : 6.86,
+//     isPass : false
+// };
+// console.log(student);
+
+// const school ={
+//     fullName : "LPU",
+//     totalStudent : 10000,
+//     totalVechile : 2000
+// };
+// console.log(school);
+
+// const profile = {
+//     username : 'enticer',
+//     post : 195,
+//     followers : 569,
+//     following : 4,
+//     isFollow : true,
+//     bio : 'Hello me shradha from apna college'
+// };
+// console.log(typeof profile["post"]);
+
+
+//Artimetic operators
+// let a = 5;
+// let b = 3;
+
+// console.log("a = ", a, "b = ", b);
+// console.log("a + b = ",a + b);
+// console.log("a - b = ",a - b);
+// console.log("a * b = ",a * b);
+// console.log("a / b = ",a / b);
+// console.log("a % b = ",a % b);
+// console.log("a ** b", a ** b); 
+
+//Unary operator
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a, "b = ", b);
+// a++; 
+// a--;
+// console.log("a++ = ", a++);
+// console.log("a =",a);
+// console.log("a-- = ", a--);
+// console.log("a =",a);
+
+//Assignment Operator
+// let a = 5;
+// let b = 2;
+
+// a **= 4;
+// console.log("a = ", a);
+//  console.log("b = ",b);
+
+//Comparsion Operator
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a); 
+
+// let a = 5;
+// let b = '5';
+
+// console.log("a !== b", a !== b);
+
+// let a =5;
+// let b = 5;
+// console.log(a <= b);
+
+//Logical operators &&
+// let a = 6;
+// let b = 5;
+
+// let cond1 = a > b;//true
+// let cond2 = a === 6;//true
+//what if when we put a = 5 where this statement is wrong
+// let cond2 = a === 5;
+
+// console.log(cond1 && cond2);
+
+//Logical operators ||
+// let a = 5;
+// let b = 4;
+//  console.log("cond1 || cond2", a < b || a === 5);
+
+//Logical operators !
+// let a = 6;
+// let b = 5;
+
+// console.log(!(a === b));
+
+//Conditional Statements
+// let age = 24;
+// if(age >= 18){
+//     console.log("You can vote");
+// }
+// if (age < 18){
+//     console.log("Your CANNOT vote");
+// }
+
