41ba8ad1ea3ddb275d7ae028befbecc625caf7bd
Aayansh Anand <aayanshanand11@gmail.com>
2025-12-24 06:55:30 +0530

    final commit

--- 16_scopes.js
+++ 16_scopes.js
@@ -20,13 +20,35 @@
 // console.log(b);
 // console.log(c);
 
-function one(){
-    const username = "Aayansh"
+// function one(){
+//     const username = "Aayansh"
 
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    two()
-}
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     two()
+// }
 // one()
+
+
+// if(true){
+//     const username = "Aayansh"
+//     if(username === "Aayansh"){
+//         const website = " Youtube"
+//         console.log(username + website);
+        
+//     }
+// }
+//In above code if we put log after the curly brashes to print the vriables we can't do it
+
+console.log(addOne(5));
+function addOne (num) {
+    return num + 1
+}
+
+
+const addTwo = function(num) {
+    return num + 2
+}
+console.log(addTwo(5));
