7b41c8531b4d7c4894e47c90591037e6cb79ff06
aayanshanand <aayanshanand11@gmail.com>
2026-03-22 18:31:26 +0530

    final commit for arrow fn

---
+++ 17_arrow.js
@@ -0,0 +1,32 @@
+// const user = {
+//     username: "Aayansh",
+//     price: 999,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username}, Welcome to website`);
+//         console.log(this);
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "Harsh"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "aayansh"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai = () => {
+//     let username = "aayansh"
+//     console.log(this);
+// }
+// chai()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3,4));
